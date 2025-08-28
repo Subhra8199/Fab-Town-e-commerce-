1b9b701e09e6f4043477f678d3fd5f7135c43edb
Subhrajeet Nath <subhrajeet123nath@gmail.com>
2025-08-28 17:18:50 +0530

    updated the item section and added the data

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../Components/Hero/Hero'
+import Popular from '../Components/popular/Popular'
 
 const Shop = () => {
-  return (
-    <div>
-      <Hero/>
-    </div>
-  )
+    return (
+        <div>
+            <Hero/>
+            <Popular/>
+        </div>
+    )
 }
 
 export default Shop
